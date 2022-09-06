26b2e1e61da224773419f5830221bb5bf6ce509b
vhhhhq <askarbekovanuraida@gmail.com>
2022-09-06 10:13:28 +0600

    all

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import {useState} from 'react';
+import Header from './components/Header/Header';
+
+
+function App() {
+  return (
+    <>
+    <Header/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Post
